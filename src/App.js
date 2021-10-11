0425054d3ba2f42a06ec9b605c8c1def4413e437
TokaAbdulhamied <tokaabdulhamied@gmail.com>
2021-10-11 11:51:11 +0200

    set up redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/layouts/Nav";
 import Dashboard from "./components/dashboard/Dashboard";
+import ProjectDetails from "./components/projects/ProjefctDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProjects";
+
 function App() {
   return (
     <Router>
-      <Nav></Nav>
-      <Route path="/" component={Dashboard} />
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/project/:id" component={ProjectDetails} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/create" component={CreateProject} />
+      </Switch>
     </Router>
   );
 }
